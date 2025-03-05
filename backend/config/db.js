263eb39640da22b91a0080fbb5b799ba8eb332e2
Santiago Serrano M <121693044+Chantilly01@users.noreply.github.com>
2025-03-05 00:25:31 -0600

    AWS_final

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,10 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  //user: 'root',
-  user: 'my_app_user',  // Cambia a 'my_app_user'
-  password: '',         // Contraseña vacía (tal como lo creaste)
+  user: 'my_app_user',
+  password: 'MiContraseñaSegura', 
   database: 'facturas_db'
 });
-
 connection.connect(err => {
   if (err) {
     console.error('Error conectando a la BD:', err);
